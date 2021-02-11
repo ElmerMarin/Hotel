9257ebe81c8fa3c79ebd7ae05289c6ad335c9006
ElmerMarin <elmermarindc@gmail.com>
2021-02-10 23:42:31 -0500

    Autenticacion

--- Hotel/Hotel.Models/Usuarios.cs
+++ Hotel/Hotel.Models/Usuarios.cs
@@ -6,16 +6,19 @@ using System.Text;
 namespace Hotel.Models
 {
     public class Usuarios
-    { 
-        [Key]
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Telefono { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool Estatus { get; set; }
-        public string UsuarioIdPadre { get; set; }
-        public string Foto { get; set; }
-        public string Puesto { get; set; }
-    }
+    {
+		[Key]
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Telefono { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public bool Estatus { get; set; }
+		public int? UsuarioIdPadre { get; set; }
+		public string Foto { get; set; }
+		public string Puesto { get; set; }
+		public DateTime Fecha { get; set; }
+
+		public string ColorTema { get; set; }
+	}
 }
