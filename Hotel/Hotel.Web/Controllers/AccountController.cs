9e8f29c5ebca391a682eb712ba380d9db70d7c65
ElmerMarin <elmermarindc@gmail.com>
2021-02-12 23:28:21 -0500

    Administrar Hoteles

--- Hotel/Hotel.Web/Controllers/AccountController.cs
+++ Hotel/Hotel.Web/Controllers/AccountController.cs
@@ -7,13 +7,13 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Hotel.Models;
+using Hotel.Tools;
 using Hotel.Toools.Services;
 using Hotel.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Logging;
 
 namespace Hotel.Web.Controllers
